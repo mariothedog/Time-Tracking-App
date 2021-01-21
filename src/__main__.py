b8c166f44c5f7c46e90f507bc2d2804f73f304d0
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-21 01:46:49 +0000

    Add event handler to log activity periodically and no longer adds iconic windows

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,41 @@
-import win32gui
 from datetime import datetime
+import os
+import sched
+import time
 
-def enum_windows_callback(hwnd, log_file):
-    if win32gui.IsWindowVisible(hwnd) and win32gui.IsIconic(hwnd):
+import win32gui
+
+DELAY_LOG_ACTIVITY = 1
+DELAY_FLUSH_LOG = 5
+
+scheduler = sched.scheduler(time.time, time.sleep)
+log_file = open("log.txt", "a")
+
+
+def enum_windows_callback(hwnd, lParam):
+    if win32gui.IsWindowVisible(hwnd) and not win32gui.IsIconic(hwnd):
         log_file.write(f"{datetime.now()}: {win32gui.GetWindowText(hwnd)}\n")
 
+
+def log_activity_action():
+    win32gui.EnumWindows(enum_windows_callback, None)
+    scheduler.enter(DELAY_LOG_ACTIVITY, 1, log_activity_action)
+
+
+def flush_log_action():
+    log_file.flush()
+    os.fsync(log_file.fileno())
+    scheduler.enter(DELAY_FLUSH_LOG, 1, flush_log_action)
+
+
 def main():
-    with open("log.txt", "a") as log_file:
-        while True:
-            win32gui.EnumWindows(enum_windows_callback, log_file)
+    try:
+        log_activity_action()
+        flush_log_action()
+        scheduler.run()
+    except:
+        log_file.close()
+
 
 if __name__ == "__main__":
     main()

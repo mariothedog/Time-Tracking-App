1fcdda57267ffbff8bab69443717078f5780af66
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-31 00:01:14 +0000

    Use QTimer instead of threading for updating the timer

--- src/__main__.py
+++ src/__main__.py
@@ -1,64 +1,18 @@
-from datetime import timedelta
-import sched
-import threading
-import time
-
-import psutil
 from PyQt5.QtWidgets import QApplication
-from win32gui import GetForegroundWindow, SetForegroundWindow, GetWindowText, GetWindowRect
-from win32process import GetWindowThreadProcessId
+from win32gui import GetForegroundWindow, SetForegroundWindow
 
 from timer_window import TimerWindow
 
-DELAY_LOG_ACTIVITY = 1
-
-WINDOW_PADDING_X_RIGHT = 30
-
-scheduler = sched.scheduler(time.time, time.sleep)
-
-app = QApplication([])
-window = TimerWindow()
-
-seconds_spent = {}
-
-
-def log_activity_action(last_hwnd=None):
-    hwnd = GetForegroundWindow()
-    name = None
-    if hwnd:
-        if GetWindowText(hwnd) == window.windowTitle():  # Prevent focus on timer window
-            SetForegroundWindow(last_hwnd)
-            hwnd = last_hwnd
-
-        rect = GetWindowRect(hwnd)
-        pos_x = rect[2] - window.width() - WINDOW_PADDING_X_RIGHT
-        pos_y = rect[1] + window.height()
-        window.move(pos_x, pos_y)
-
-        pid = GetWindowThreadProcessId(hwnd)[1]
-        process = psutil.Process(pid)
-
-        name = process.name()
-        if name in seconds_spent:
-            seconds_spent[name] += 1
-        else:
-            seconds_spent[name] = 0
-        seconds = seconds_spent[name]
-        window.label.setText(str(timedelta(seconds=seconds)))
-
-    scheduler.enter(DELAY_LOG_ACTIVITY, 1, lambda: log_activity_action(hwnd))
-
 
 def main():
+    app = QApplication([])
+    window = TimerWindow()
+
     temp_hwnd = GetForegroundWindow()
     window.show()
     window.clearFocus()
     SetForegroundWindow(temp_hwnd)
 
-    log_activity_action()
-
-    scheduler_thread = threading.Thread(target=scheduler.run)
-    scheduler_thread.start()
     app.exec_()
 
 
--- src/timer_window.py
+++ src/timer_window.py
@@ -1,9 +1,21 @@
-from PyQt5.QtCore import Qt
+from datetime import timedelta
+
+import psutil
+
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QMainWindow, QLabel
 
+from win32gui import GetForegroundWindow, SetForegroundWindow, GetWindowText, GetWindowRect
+from win32process import GetWindowThreadProcessId
+
 
 class TimerWindow(QMainWindow):
+    INTERVAL_UPDATE_WINDOW = 1000
+    WINDOW_PADDING_X_RIGHT = 30
+
+    seconds_spent = {}
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -17,5 +29,37 @@ class TimerWindow(QMainWindow):
         self.label.setAlignment(Qt.AlignCenter)
         self.setCentralWidget(self.label)
 
+        self.update_window()
+
+        self.update_window_timer = QTimer(self)
+        self.update_window_timer.timeout.connect(self.update_window)
+        self.update_window_timer.start(self.INTERVAL_UPDATE_WINDOW)
+
     def closeEvent(self, event):
         event.ignore()
+
+    def update_window(self):
+        hwnd = GetForegroundWindow()
+        name = None
+        if hwnd:
+            if GetWindowText(hwnd) == self.windowTitle():  # Prevent focus on timer window
+                SetForegroundWindow(self.last_hwnd)
+                hwnd = self.last_hwnd
+
+            rect = GetWindowRect(hwnd)
+            pos_x = rect[2] - self.width() - self.WINDOW_PADDING_X_RIGHT
+            pos_y = rect[1] + self.height()
+            self.move(pos_x, pos_y)
+
+            pid = GetWindowThreadProcessId(hwnd)[1]
+            process = psutil.Process(pid)
+
+            name = process.name()
+            if name in self.seconds_spent:
+                self.seconds_spent[name] += 1
+            else:
+                self.seconds_spent[name] = 0
+            seconds = self.seconds_spent[name]
+            self.label.setText(str(timedelta(seconds=seconds)))
+
+        self.last_hwnd = hwnd
